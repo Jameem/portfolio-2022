a7be4824952cf77b928deced1c639ffb0af6d388
Jameem <jameemkurikkal.mp@gmail.com>
2024-03-24 11:13:06 +0100

    feat: add earth to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -25,6 +25,26 @@ const Portfolio = () => {
                         <img src="img/thumbs/1-1.jpg" alt="" />
                         <div
                           className="main"
+                          data-img-url="img/portfolio/earth.jpg"
+                        />
+                      </div>
+                      <div className="details">
+                        <h3>Earth</h3>
+                        <span>Powered by ThreeJS</span>
+                      </div>
+                      <a
+                        className="edrea_tm_full_link"
+                        href="https://earth-mj.netlify.app/"
+                        target="_blank"
+                      />
+                    </div>
+                  </SwiperSlide>
+                <SwiperSlide className="swiper-slide">
+                    <div className="list_inner">
+                      <div className="image">
+                        <img src="img/thumbs/1-1.jpg" alt="" />
+                        <div
+                          className="main"
                           data-img-url="img/portfolio/ai-text-to-voice.jpg"
                         />
                       </div>
